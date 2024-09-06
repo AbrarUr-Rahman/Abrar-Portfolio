3ef2ebb462ee8fd4c3b1bf6cbb73268d6a400eca
AbrarUr-Rahman <ishmam000@gmail.com>
2024-09-06 17:21:24 +0600

    Contact Done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,5 +1,8 @@
 import "./Contact.css";
 import theme_pattern from "../../assets/theme_pattern.svg";
+import mail_icon from "../../assets/mail_icon.svg";
+import location_icon from "../../assets/location_icon.svg";
+import call_icon from "../../assets/call_icon.svg";
 
 const Contact = () => {
   return (
@@ -7,6 +10,44 @@ const Contact = () => {
       <div className="contact-title">
         <h1>Get In Touch</h1>
         <img src={theme_pattern} alt="theme_pattern" />
+      </div>
+      <div className="contact-section">
+        <div className="contact-left">
+          <h1>Let's Talk</h1>
+          <p>
+            I'm currently available to do freelance jobs and projects. Feel free
+            to contact whenever you want to realize your plans
+          </p>
+          <div className="contact-details">
+            <div className="contact-detail">
+              <img src={mail_icon} alt="" />
+              <p>ishmam000@gmail.com</p>
+            </div>
+            <div className="contact-detail">
+              <img src={call_icon} alt="" />
+              <p>01760253194</p>
+            </div>
+            <div className="contact-detail">
+              <img src={location_icon} alt="" />
+              <p>Mirpur,Dhaka</p>
+            </div>
+          </div>
+        </div>
+        <form className="contact-right">
+          <label htmlFor="">Your Name</label>
+          <input type="text" placeholder="Enter your Name" name="name" />
+          <label htmlFor="">Your Email</label>
+          <input type="email" placeholder="Enter your Email" name="email" />
+          <label htmlFor="">Write your Message Here</label>
+          <textarea
+            name="message"
+            rows="8"
+            placeholder="Enter your message"
+          ></textarea>
+          <button type="submit" className="contact-submit">
+            Submit Now
+          </button>
+        </form>
       </div>
     </div>
   );
